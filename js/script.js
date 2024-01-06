fa1fbf1013a971046c035348ec14593ebe53cc95
alannofrialdi <alannofrialdi098@gmail.com>
2024-01-06 17:55:06 +0700

    modif dikit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,47 @@
-window.addEventListener("load", () => {
-  const loaderOverlay = document.querySelector(".loader-overlay");
-  const loader = document.querySelector(".loader");
-  loaderOverlay.style.opacity = 1;
-  setTimeout(() => {
-    loaderOverlay.style.opacity = 0;
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Dom loaded');
+})
+
+const loaders = document.querySelector('.loader-overlay')
+// Function to handle the complete loading of the page
+const handlePageLoad = () => {
+  console.log("Window loaded");
+  
+  // Display loaders initially
+  loaders.style.opacity = 1;
+
+    // Create a Promise that resolves when all resources are loaded
+    const allResourcesLoaded = new Promise((resolve) => {
+      const checkIfLoaded = () => {
+        if (
+          document.readyState === "complete" &&
+          (!document.hidden || document.visibilityState === "prerender")
+        ) {
+          console.log("All resources loaded");
+          resolve();
+        } else {
+          setTimeout(checkIfLoaded, 100);
+        }
+      };
+  
+      checkIfLoaded();
+    });
+
+  // When all resources are loaded, perform the transition
+  allResourcesLoaded.then(() => {
     setTimeout(() => {
-      loaderOverlay.classList.add('hidden');
-    }, 500);
-  }, 3000);
-});
+      // Apply fade-out transition
+      loaders.style.opacity = 0;
+
+      // Hide loaders and display other elements after the transition is complete
+      setTimeout(() => {
+        loaders.style.display = "none";
+      }, 500); // Adjust the time based on your desired transition duration
+    }, 2500); // Adjust the time based on your desired delay
+  });
+};
+// Attach the handlePageLoad function to the window.onload event
+window.onload = handlePageLoad;
 
 // Helper function to toggle active class
 const toggleActiveClass = (element, className) => {
