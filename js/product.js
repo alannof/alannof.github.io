8c12971b4cfb78fc65038a5a4dbabab270a3f988
alannofrialdi <alannofrialdi098@gmail.com>
2024-01-06 07:47:50 +0700

    add new features

--- js/product.js
+++ js/product.js
@@ -1,4 +1,26 @@
-var cardSlider = new Swiper(".card-slider", {
+const secondCSS = document.getElementById("secondCSS");
+const loaders = document.getElementById("loaders");
+const product = document.getElementById("product");
+
+window.addEventListener("load", () => {
+  // Display loaders initially
+  loaders.style.opacity = 1;
+
+  setTimeout(() => {
+    createLink();
+    // Apply fade-out transition
+    loaders.style.opacity = 0;
+
+    // Hide loaders and display other elements after the transition is complete
+    setTimeout(() => {
+      loaders.style.display = "none";
+      product.classList.remove("hidden");
+      secondCSS.remove();
+    }, 500); // Adjust the time based on your desired transition duration
+  }, 3000);
+});
+
+const cardSlider = new Swiper(".card-slider", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
@@ -19,3 +41,10 @@ var cardSlider = new Swiper(".card-slider", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const createLink = () => {
+  const load = document.createElement("link");
+  load.setAttribute("rel", "stylesheet");
+  load.setAttribute("href", "css/product.css");
+  document.head.appendChild(load);
+};
--- js/script.js
+++ js/script.js
@@ -1,72 +1,96 @@
-// * toggle active hamburger
-const navbarNav = document.querySelector(".navbar-nav");
-
-document.querySelector("#ham-menu").onclick = (e) => {
-  navbarNav.classList.toggle("active");
-  e.preventDefault();
+// Helper function to toggle active class
+const toggleActiveClass = (element, className) => {
+  element.classList.toggle(className);
 };
 
-// note: klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#ham-menu");
+// Event listener to toggle active class for hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#ham-menu").addEventListener("click", (e) => {
+  toggleActiveClass(navbarNav, "active");
+  e.preventDefault();
+});
 
-document.addEventListener("click", function (e) {
+// Event listener to hide navbar when clicking outside
+document.addEventListener("click", (e) => {
+  const hamburger = document.querySelector("#ham-menu");
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });
 
-// * toggle active search form
+// Event listener to toggle active class for search form
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
-
-document.querySelector("#search-button").onclick = (e) => {
-  searchForm.classList.toggle("active");
+document.querySelector("#search-button").addEventListener("click", (e) => {
+  toggleActiveClass(searchForm, "active");
   searchBox.focus();
   e.preventDefault();
-};
+});
 
-// note: klik diluar search-box untuk menghilangkan search
-const search = document.querySelector("#search-button");
-document.addEventListener("click", function (e) {
-  if (!search.contains(e.target) && !searchForm.contains(e.target)) {
+// Event listener to hide search form when clicking outside
+const searchButton = document.querySelector("#search-button");
+document.addEventListener("click", (e) => {
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
   }
 });
 
-// * toggle active cart
+// Event listener to toggle active class for shopping cart
 const shoppingCart = document.querySelector(".shopping-cart");
-document.querySelector("#cart-button").onclick = (e) => {
-  shoppingCart.classList.toggle("active");
+document.querySelector("#cart-button").addEventListener("click", (e) => {
+  toggleActiveClass(shoppingCart, "active");
   e.preventDefault();
-};
+});
 
-// note: klik diluar cart untuk menghilangkan cart
-const cart = document.querySelector("#cart-button");
-document.addEventListener("click", function (e) {
-  if (!cart.contains(e.target) && !shoppingCart.contains(e.target)) {
+// Event listener to hide shopping cart when clicking outside
+const cartButton = document.querySelector("#cart-button");
+document.addEventListener("click", (e) => {
+  if (!cartButton.contains(e.target) && !shoppingCart.contains(e.target)) {
     shoppingCart.classList.remove("active");
   }
 });
 
-// * modal box
+// Event listener to show item detail modal
 const itemDetailModal = document.querySelector("#item-detail-modal");
 const itemDetailButtons = document.querySelectorAll(".detail-button");
 
 itemDetailButtons.forEach((btn) => {
-  btn.onclick = (e) => {
+  btn.addEventListener("click", (e) => {
     itemDetailModal.style.display = "flex";
     e.preventDefault();
-  };
+  });
 });
 
-// note: untuk menghilangkan modal lewat X
-document.querySelector(".modal .close-icon").onclick = (e) => {
+// Event listener to hide item detail modal when clicking the close icon or outside
+document.querySelector(".modal .close-icon").addEventListener("click", (e) => {
   itemDetailModal.style.display = "none";
   e.preventDefault();
-};
-// untuk menghilangkan modal dimana saja
-window.onclick = (e) => {
+});
+
+window.addEventListener("click", (e) => {
   if (e.target === itemDetailModal) {
     itemDetailModal.style.display = "none";
   }
-};
+});
+
+const input = document.querySelector('input[type="search"]');
+input.addEventListener("keyup", (e) => {
+  const sbv = input.value.toLowerCase();
+  console.log(sbv);
+  switch (sbv) {
+    case "about":
+      window.location.assign("#about");
+      break;
+    case "contact":
+      window.location.assign("#contact");
+      break;
+    case "product":
+      window.location.assign("/product.html");
+      break;
+    case "best":
+      window.location.assign("#seller");
+      break;
+  }
+  // e.preventDefault();
+});
+
